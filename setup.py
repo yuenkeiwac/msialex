768d982c4af20807f840d7647f9d8f0c10f639ea
yuenkeiwac <97879052+yuenkeiwac@users.noreply.github.com>
2022-01-18 20:47:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import find_packages, setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='msiaLex',
     packages=find_packages(include=['msiaLex']),
-    version='0.2.0',
+    version='0.2.1',
     description='Malaysia Lexicon',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Khor Yuen Kei',
     author_email='khoryk1104@gmail.com',
     url='https://github.com/yuenkeiwac/msialex',
