6b8198326f566730668889ff3f20843e9c396ccb
yuenkeiwac <97879052+yuenkeiwac@users.noreply.github.com>
2022-01-18 16:23:34 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='msiaLex',
     packages=find_packages(include=['msiaLex']),
-    version='0.1.0',
+    version='0.2.0',
     description='Malaysia Lexicon',
     author='Khor Yuen Kei',
     author_email='khoryk1104@gmail.com',
