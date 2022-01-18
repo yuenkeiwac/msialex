f7d6a0c38348f8b4992af3eb2aa632544c227e28
yuenkeiwac <97879052+yuenkeiwac@users.noreply.github.com>
2022-01-18 16:20:11 +0800

    Update modifier.py

--- msiaLex/modifier.py
+++ msiaLex/modifier.py
@@ -244,3 +244,31 @@ def disjunction(lang):
                 '不料': {'intensity': 3, 'tag': 'c'},
                 '岂知': {'intensity': 3, 'tag': 'c'}}
         # return ['但', '但是', '却', '然而', '而', '偏偏', '只是', '就是', '不过', '可是', '不料', '岂知']
+
+def stopword(lang):
+    if(lang == 'en'):
+        return ['a','about','above','across','after','afterwards','again','against','all','almost','alone','along','already','also','although','always','am',
+                'among','amongst','amoungst','amount','an','and','another','any','anyhow','anyone','anything','anyway','anywhere','are','around','as','at',
+                'back','be','became','because','become','becomes','becoming','been','before','beforehand','behind','being','below','beside','besides','between','beyond','bill','both','bottom','but','by',
+                'can','cannot','cant','co','could','couldnt',
+                'de','did','didn','do','does','doesn','doing','don','done','down','due','during',
+                'each','eg','eight','either','eleven','else','elsewhere','etc','even','ever','every','everyone','everything','everywhere','except',
+                'few','fifteen','fifty','fill','find','first','five','for','former','formerly','forty','found','four','from','front','further',
+                'had','has','hasnt','have','he','hence','her','here','hereafter','hereby','herein','hereupon','hers','herself','him','himself','his','how','however','hundred',
+                'i','ie','if','in','inc','indeed','into','is','it','its','itself',
+                'just',
+                'keep','kg','km',
+                'last','latter','latterly','least','less','ltd',
+                'made','make','many','may','me','meanwhile','might','mill','mine','more','moreover','most','mostly','move','much','must','my','myself',
+                'name','namely','neither','never','nevertheless','next','nine','no','nobody','none','noone','nor','not','nothing','now','nowhere',
+                'of','off','often',
+                'on','once','one','only','onto','or','other','others','otherwise','our','ours','ourselves','out','over','own',
+                'part','per','perhaps','please','put',
+                'quite',
+                'rather','re','really','regarding',
+                'same','say','see','seem','seemed','seeming','seems','several','she','should','side','since','six','sixty','so','some','somehow','someone','something','sometime','sometimes','somewhere','still','such',
+                'ten','than','that','the','their','them','themselves','then','thence','there','thereafter','thereby','therefore','therein','thereupon','these','they','third','this','those','though','three','through','throughout','thru','thus','to','together','too','toward','towards','twelve','twenty','two',
+                'un','under','unless','until','up','upon','us','used','using',
+                'various','very','via',
+                'was','we','were','what','whatever','when','whence','whenever','where','whereafter','whereas','whereby','wherein','whereupon','wherever','whether','which','while','whither','who','whoever','whole','whom','whose','why','will','with','within','without','would',
+                'yet','you','your','yours','yourself','yourselves']
