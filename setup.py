10704be879f8e948ff1a2e3fb7b6106a8f7d549f
yuenkeiwac <97879052+yuenkeiwac@users.noreply.github.com>
2022-01-18 11:04:53 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+setup(
+    name='msiaLex',
+    packages=find_packages(include=['msiaLex']),
+    version='0.1.0',
+    description='Malaysia Lexicon',
+    author='Khor Yuen Kei',
+    author_email='khoryk1104@gmail.com'
+    license='MIT',
+    install_requires=[], 
+    setup_requires=['pytest-runner'], 
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests'
+)
